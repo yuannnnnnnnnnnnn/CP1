5337b43723faee30ff7351cdb11a2e2b73b47713
Nerissa-W <nerissa.wang@mail.utoronto.ca>
2025-02-08 16:10:35 -0500

    Uncommented code for PythonTA and removed a TODO comment

--- project1/proj1_simulation.py
+++ project1/proj1_simulation.py
@@ -137,11 +137,11 @@ if __name__ == "__main__":
     # When you are ready to check your work with python_ta, uncomment the following lines.
     # (Delete the "#" and space before each line.)
     # IMPORTANT: keep this code indented inside the "if __name__ == '__main__'" block
-    # import python_ta
-    # python_ta.check_all(config={
-    #     'max-line-length': 120,
-    #     'disable': ['R1705', 'E9998', 'E9999']
-    # })
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 120,
+        'disable': ['R1705', 'E9998', 'E9999']
+    })
 
     # TODO: Modify the code below to provide a walkthrough of commands needed to win and lose the game
     win_walkthrough = []  # Create a list of all the commands needed to walk through your game to win it
